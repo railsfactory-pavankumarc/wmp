6bd1815401300233ab515aba081018f84c8821aa
Pavan <pavankumar@railsfactory.org>
2014-08-25 19:52:05 +0530

    modified cart

--- cart/depot/app/models/product.rb
+++ cart/depot/app/models/product.rb
@@ -6,7 +6,7 @@ validates :price, numericality: {greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
 validates :image_url, allow_blank: true, format: {
 with:
-%r{\.(gif|jpg|png)\Z}i,
+%r{\.(gif|jpg|png|jpeg)\Z}i,
 message: 'must be a URL for GIF, JPG or PNG image.'
 }
 
