edb3500813ac3300b7c5ca0b7f66930c5c374548
Pavan <pavankumar@railsfactory.org>
2014-08-26 20:58:49 +0530

    amid controls added

--- cart/depot/config/routes.rb
+++ cart/depot/config/routes.rb
@@ -1,16 +1,30 @@
-Rails.application.routes.draw do
+Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+  get 'login' => :new
+  post 'login' => :create
+  delete 'logout' => :destroy
+end
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
+
   resources :orders
 
   resources :line_items
 
   resources :carts
+  
+  get 'store/index'
   resources :products do
      get :who_bought, on: :member
   end
 
-  get 'store/index'
+  
 
-  resources :products
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- cart/depot/test/test_helper.rb
+++ cart/depot/test/test_helper.rb
@@ -4,7 +4,19 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+	fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+
+	# Add more helper methods to be used by all tests here...
+	def login_as(user)
+		session[:user_id] = users(user).id
+	end
+	def logout
+		session.delete :user_id
+	end
+	def setup
+		login_as :one if defined? session
+	end
+
+  
 end
