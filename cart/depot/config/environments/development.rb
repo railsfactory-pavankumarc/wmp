b4222f19feba15cefc7a1f408e4b80e03e111406
Pavan <pavankumar@railsfactory.org>
2014-08-26 18:35:08 +0530

    added action mailer functionality

--- cart/depot/config/environments/development.rb
+++ cart/depot/config/environments/development.rb
@@ -34,4 +34,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+Depot::Application.configure do
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'pavankumar@railsfactory.org',
+  password:             '23pavkumcha1992',
+  authentication:       'plain',
+  #enable_starttls_auto: true  
+}
+end
 end
