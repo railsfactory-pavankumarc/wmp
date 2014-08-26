9c292d007d749242fcdd31cb81f6e494c7df8f02
Pavan <pavankumar@railsfactory.org>
2014-08-26 16:03:22 +0530

    created a form to capture details for the order

--- cart/depot/config/routes.rb
+++ cart/depot/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
+  resources :products do
+     get :who_bought, on: :member
+  end
 
   get 'store/index'
 
